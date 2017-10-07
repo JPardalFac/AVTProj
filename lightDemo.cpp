120c71e73e3e4d399f85bf345425cdb43b251815
JPardalFac <JPardalFac@users.noreply.github.com>
2017-10-07 11:24:13 +0100

    Initial commit

---
+++ lightDemo.cpp
@@ -0,0 +1,574 @@
+//
+// AVT demo light 
+// based on demos from GLSL Core Tutorial in Lighthouse3D.com   
+//
+// This demo was built for learning purposes only.
+// Some code could be severely optimised, but I tried to
+// keep as simple and clear as possible.
+//
+// The code comes with no warranties, use it at your own risk.
+// You may use it, or parts of it, wherever you want.
+//
+
+#include <math.h>
+#include <iostream>
+#include <sstream>
+
+#include <string>
+
+// include GLEW to access OpenGL 3.3 functions
+#include <GL/glew.h>
+
+// GLUT is the toolkit to interface with the OS
+#include <GL/freeglut.h>
+
+// Use Very Simple Libs
+#include "VSShaderlib.h"
+#include "AVTmathLib.h"
+#include "VertexAttrDef.h"
+#include "basic_geometry.h"
+
+#define CAPTION "AVT Light Demo"
+int WindowHandle = 0;
+int WinX = 640, WinY = 480;
+
+unsigned int FrameCount = 0;
+
+
+
+VSShaderLib shader;
+
+const int numObjs = 10;
+struct MyMesh mesh[numObjs];
+int objId = 0; //id of the object mesh - to be used as index of mesh: mesh[objID] means the current mesh
+
+//wheels deviations from car body
+float dev = 0.5f;
+float xpos[4] = {0.5f+dev,0.5f+dev,-0.5f+dev,-0.5f+dev};
+float ypos[4] = { 0.5f+dev,-0.5f+dev,0.5f+dev,-0.5f+dev };
+
+//External array storage defined in AVTmathLib.cpp
+
+/// The storage for matrices
+extern float mMatrix[COUNT_MATRICES][16];
+extern float mCompMatrix[COUNT_COMPUTED_MATRICES][16];
+
+/// The normal matrix
+extern float mNormal3x3[9];
+
+GLint pvm_uniformId;
+GLint vm_uniformId;
+GLint normal_uniformId;
+GLint lPos_uniformId;
+
+// Cameras Position
+float camX, camY, camZ;
+float FOcamX, FOcamY, FOcamZ;
+float FPcamX, FPcamY, FPcamZ;
+
+// Mouse Tracking Variables
+int startX, startY, tracking = 0;
+
+// Camera Spherical Coordinates
+float alpha = 39.0f, beta = 51.0f;
+float r = 10.0f;
+
+// Frame counting and FPS computation
+long myTime, timebase = 0, frame = 0;
+char s[32];
+float lightPos[4] = { 4.0f, 6.0f, 2.0f, 1.0f };
+
+void drawSnitch();	//early declaration of the function
+
+float angle = 0;
+
+void Timer(int value) 
+{
+	angle += 2.5f;
+	glutPostRedisplay();
+	glutTimerFunc(100, Timer, 0);
+}
+
+void timer(int value)
+{
+	std::ostringstream oss;
+	oss << CAPTION << ": " << FrameCount << " FPS @ (" << WinX << "x" << WinY << ")";
+	std::string s = oss.str();
+	glutSetWindow(WindowHandle);
+	glutSetWindowTitle(s.c_str());
+	FrameCount = 0;
+	glutTimerFunc(1000, timer, 0);
+}
+
+void refresh(int value)
+{
+	glutPostRedisplay();
+	glutTimerFunc(1000 / 60, refresh, 0);
+}
+
+// ------------------------------------------------------------
+//
+// Reshape Callback Function
+//
+
+void changeSize(int w, int h) {
+
+	float ratio;
+	// Prevent a divide by zero, when window is too short
+	if (h == 0)
+		h = 1;
+	// set the viewport to be the entire window
+	glViewport(0, 0, w, h);
+	// set the projection matrix
+	ratio = (1.0f * w) / h;
+	loadIdentity(PROJECTION);
+	perspective(53.13f, ratio, 0.1f, 1000.0f);
+}
+
+
+// ------------------------------------------------------------
+//
+// Render stufff
+//
+
+
+void sendMaterial(int index) {
+	GLint loc;
+	loc = glGetUniformLocation(shader.getProgramIndex(), "mat.ambient");
+	glUniform4fv(loc, 1, mesh[index].mat.ambient);
+	loc = glGetUniformLocation(shader.getProgramIndex(), "mat.diffuse");
+	glUniform4fv(loc, 1, mesh[index].mat.diffuse);
+	loc = glGetUniformLocation(shader.getProgramIndex(), "mat.specular");
+	glUniform4fv(loc, 1, mesh[index].mat.specular);
+	loc = glGetUniformLocation(shader.getProgramIndex(), "mat.shininess");
+	glUniform1f(loc, mesh[index].mat.shininess);
+}
+
+void drawObj(int index) {
+	glBindVertexArray(mesh[index].vao);
+	glDrawElements(mesh[index].type, mesh[index].numIndexes, GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
+}
+
+void sendMatrices() {
+	computeDerivedMatrix(PROJ_VIEW_MODEL);
+	glUniformMatrix4fv(vm_uniformId, 1, GL_FALSE, mCompMatrix[VIEW_MODEL]);
+	glUniformMatrix4fv(pvm_uniformId, 1, GL_FALSE, mCompMatrix[PROJ_VIEW_MODEL]);
+	computeNormalMatrix3x3();
+	glUniformMatrix3fv(normal_uniformId, 1, GL_FALSE, mNormal3x3);
+}
+
+void renderScene(void) {
+
+
+	FrameCount++;
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	// load identity matrices
+	loadIdentity(VIEW);
+	loadIdentity(MODEL);
+	// set the camera using a function similar to gluLookAt
+	lookAt(camX, camY, camZ, 0, 0, 0, 0, 1, 0);
+	// use our shader
+	glUseProgram(shader.getProgramIndex());
+
+	//send the light position in eye coordinates
+
+		//glUniform4fv(lPos_uniformId, 1, lightPos); //efeito capacete do mineiro, ou seja lighPos foi definido em eye coord 
+
+	float res[4];
+	multMatrixPoint(VIEW, lightPos, res);   //lightPos definido em World Coord so is converted to eye space
+	glUniform4fv(lPos_uniformId, 1, res);
+
+
+	//draw car body
+	sendMaterial(0);
+	pushMatrix(MODEL);
+	translate(MODEL, 2.0f, 0.0f, 2.0f);
+	rotate(MODEL, angle, 0, 90, 0);
+	sendMatrices();
+	drawObj(0);
+
+	//draw car wheels
+	int x=0, y=0;
+	for (int i = 1; i <= 4; i++) {
+
+		pushMatrix(MODEL);
+		sendMaterial(i);
+		translate(MODEL, xpos[x], 0.0f, ypos[y]);
+		rotate(MODEL, 90,0,0,1);
+		sendMatrices();
+		drawObj(i);
+
+		popMatrix(MODEL);
+
+		x++;
+		y++;
+
+	}
+	//draw table
+	popMatrix(MODEL);
+	pushMatrix(MODEL);
+	sendMaterial(5);
+	translate(MODEL, 2.0f, -0.1f, 2.0f);
+	rotate(MODEL,-90,1,0,0);
+	sendMatrices();
+	drawObj(5);
+	popMatrix(MODEL);
+
+	drawSnitch();
+
+	glutSwapBuffers();
+}
+
+// ------------------------------------------------------------
+//
+// Events from the Keyboard
+//
+
+void processKeys(unsigned char key, int xx, int yy)
+{
+	switch (key) {
+
+	case 27:
+		glutLeaveMainLoop();
+		break;
+
+	case 'c':
+		printf("Camera Spherical Coordinates (%f, %f, %f)\n", alpha, beta, r);
+		break;
+	case 'm': glEnable(GL_MULTISAMPLE); break;
+	case 'n': glDisable(GL_MULTISAMPLE); break;
+	}
+}
+
+
+// ------------------------------------------------------------
+//
+// Mouse Events
+//
+
+void processMouseButtons(int button, int state, int xx, int yy)
+{
+	// start tracking the mouse
+	if (state == GLUT_DOWN) {
+		startX = xx;
+		startY = yy;
+		if (button == GLUT_LEFT_BUTTON)
+			tracking = 1;
+		else if (button == GLUT_RIGHT_BUTTON)
+			tracking = 2;
+	}
+
+	//stop tracking the mouse
+	else if (state == GLUT_UP) {
+		if (tracking == 1) {
+			alpha -= (xx - startX);
+			beta += (yy - startY);
+		}
+		else if (tracking == 2) {
+			r += (yy - startY) * 0.01f;
+			if (r < 0.1f)
+				r = 0.1f;
+		}
+		tracking = 0;
+	}
+}
+
+// Track mouse motion while buttons are pressed
+
+void processMouseMotion(int xx, int yy)
+{
+
+	int deltaX, deltaY;
+	float alphaAux, betaAux;
+	float rAux;
+
+	deltaX = -xx + startX;
+	deltaY = yy - startY;
+
+	// left mouse button: move camera
+	if (tracking == 1) {
+
+
+		alphaAux = alpha + deltaX;
+		betaAux = beta + deltaY;
+
+		if (betaAux > 85.0f)
+			betaAux = 85.0f;
+		else if (betaAux < -85.0f)
+			betaAux = -85.0f;
+		rAux = r;
+	}
+	// right mouse button: zoom
+	else if (tracking == 2) {
+
+		alphaAux = alpha;
+		betaAux = beta;
+		rAux = r + (deltaY * 0.01f);
+		if (rAux < 0.1f)
+			rAux = 0.1f;
+	}
+
+	camX = rAux * sin(alphaAux * 3.14f / 180.0f) * cos(betaAux * 3.14f / 180.0f);
+	camZ = rAux * cos(alphaAux * 3.14f / 180.0f) * cos(betaAux * 3.14f / 180.0f);
+	camY = rAux *   						       sin(betaAux * 3.14f / 180.0f);
+
+	//  uncomment this if not using an idle func
+	//	glutPostRedisplay();
+}
+
+
+void mouseWheel(int wheel, int direction, int x, int y) {
+
+	r += direction * 0.1f;
+	if (r < 0.1f)
+		r = 0.1f;
+
+	camX = r * sin(alpha * 3.14f / 180.0f) * cos(beta * 3.14f / 180.0f);
+	camZ = r * cos(alpha * 3.14f / 180.0f) * cos(beta * 3.14f / 180.0f);
+	camY = r *   						     sin(beta * 3.14f / 180.0f);
+
+	//  uncomment this if not using an idle func
+	//	glutPostRedisplay();
+}
+
+// --------------------------------------------------------
+//
+// Shader Stuff
+//
+
+
+GLuint setupShaders() {
+
+	// Shader for models
+	shader.init();
+	shader.loadShader(VSShaderLib::VERTEX_SHADER, "C:\\Users\\Faculdade.JOAOPARDALLAPTO\\Documents\\AVT\\Lab02_0210\\Lab02\\shaders\\pointlight.vert");
+	shader.loadShader(VSShaderLib::FRAGMENT_SHADER, "C:\\Users\\Faculdade.JOAOPARDALLAPTO\\Documents\\AVT\\Lab02_0210\\Lab02\\shaders\\pointlight.frag");
+
+	// set semantics for the shader variables
+	glBindFragDataLocation(shader.getProgramIndex(), 0, "colorOut");
+	glBindAttribLocation(shader.getProgramIndex(), VERTEX_COORD_ATTRIB, "position");
+	glBindAttribLocation(shader.getProgramIndex(), NORMAL_ATTRIB, "normal");
+	glBindAttribLocation(shader.getProgramIndex(), TEXTURE_COORD_ATTRIB, "texCoord");
+
+	glLinkProgram(shader.getProgramIndex());
+
+	pvm_uniformId = glGetUniformLocation(shader.getProgramIndex(), "m_pvm");
+	vm_uniformId = glGetUniformLocation(shader.getProgramIndex(), "m_viewModel");
+	normal_uniformId = glGetUniformLocation(shader.getProgramIndex(), "m_normal");
+	lPos_uniformId = glGetUniformLocation(shader.getProgramIndex(), "l_pos");
+
+	printf("InfoLog for Hello World Shader\n%s\n\n", shader.getAllInfoLogs().c_str());
+
+	return(shader.isProgramLinked());
+}
+
+void createCar() {
+
+	float amb[] = { 0.2f, 0.15f, 0.1f, 1.0f };
+	float diff[] = { 0.8f, 0.6f, 0.4f, 1.0f };
+	float spec[] = { 0.8f, 0.8f, 0.8f, 1.0f };
+	float emissive[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+	float shininess = 100.0f;
+	int texcount = 0;
+
+	objId = 0;
+	memcpy(mesh[objId].mat.ambient, amb, 4 * sizeof(float));
+	memcpy(mesh[objId].mat.diffuse, diff, 4 * sizeof(float));
+	memcpy(mesh[objId].mat.specular, spec, 4 * sizeof(float));
+	memcpy(mesh[objId].mat.emissive, emissive, 4 * sizeof(float));
+	mesh[objId].mat.shininess = shininess;
+	mesh[objId].mat.texCount = texcount;
+	createCube();
+
+	//WHEELS//	
+	objId = 1;
+	memcpy(mesh[objId].mat.ambient, amb, 4 * sizeof(float));
+	memcpy(mesh[objId].mat.diffuse, diff, 4 * sizeof(float));
+	memcpy(mesh[objId].mat.specular, spec, 4 * sizeof(float));
+	memcpy(mesh[objId].mat.emissive, emissive, 4 * sizeof(float));
+	mesh[objId].mat.shininess = shininess;
+	mesh[objId].mat.texCount = texcount;
+	createTorus(0.1, 0.2, 15, 5);
+
+	objId = 2;
+	memcpy(mesh[objId].mat.ambient, amb, 4 * sizeof(float));
+	memcpy(mesh[objId].mat.diffuse, diff, 4 * sizeof(float));
+	memcpy(mesh[objId].mat.specular, spec, 4 * sizeof(float));
+	memcpy(mesh[objId].mat.emissive, emissive, 4 * sizeof(float));
+	mesh[objId].mat.shininess = shininess;
+	mesh[objId].mat.texCount = texcount;
+	createTorus(0.1, 0.2, 15, 5);
+
+	objId = 3;
+	memcpy(mesh[objId].mat.ambient, amb, 4 * sizeof(float));
+	memcpy(mesh[objId].mat.diffuse, diff, 4 * sizeof(float));
+	memcpy(mesh[objId].mat.specular, spec, 4 * sizeof(float));
+	memcpy(mesh[objId].mat.emissive, emissive, 4 * sizeof(float));
+	mesh[objId].mat.shininess = shininess;
+	mesh[objId].mat.texCount = texcount;
+	createTorus(0.1, 0.2, 15, 5);
+
+	objId = 4;
+	memcpy(mesh[objId].mat.ambient, amb, 4 * sizeof(float));
+	memcpy(mesh[objId].mat.diffuse, diff, 4 * sizeof(float));
+	memcpy(mesh[objId].mat.specular, spec, 4 * sizeof(float));
+	memcpy(mesh[objId].mat.emissive, emissive, 4 * sizeof(float));
+	mesh[objId].mat.shininess = shininess;
+	mesh[objId].mat.texCount = texcount;
+	createTorus(0.1, 0.2, 15, 5);
+}
+
+void copyArrayOfFloats(float amb[], float dif[], float spec[], float emi[], float shin, MyMesh &toThis) {
+	memcpy(toThis.mat.ambient, amb, sizeof(float) * 4);
+	memcpy(toThis.mat.diffuse, dif, sizeof(float) * 4);
+	memcpy(toThis.mat.specular, spec, sizeof(float) * 4);
+	memcpy(toThis.mat.emissive, emi, sizeof(float) * 4);
+	toThis.mat.shininess = shin;
+}
+
+void createSnitch() 
+{
+	//define the material components for the snitch
+	float ambient[] = {0.9f, 0.5f, 0.0f, 1.0f };
+	float diffuse[] = { 0.9f, 0.5f, 0.0f, 1.0f };
+	float specular[] = { 0.9f, 0.4f, 0.0f, 1.0f };
+	float emissive[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+	float shininess = 100.0f;
+
+	//body 
+	objId = 6; //THIS IS WROOOOOOOOOONG , it should be something like objId++; can't be 5 because of the tabletop
+	//objId++; //add a objId to be used on the mesh 
+	copyArrayOfFloats(ambient, diffuse, specular, emissive, shininess, mesh[objId]);	//copy materials to its mesh.materials component to be used by the shader
+	//createSphere(1, 20); //create the shape of each component
+	createTorus(.3, .6, 15, 5);
+	//wing1
+	objId = 7;
+	copyArrayOfFloats(ambient, diffuse, specular, emissive, shininess, mesh[objId]);
+	createCone(1.0f, 1.0f, 10);
+}
+
+void drawSnitch()
+{
+	//body
+	sendMaterial(6);					//send materials to the shader
+	pushMatrix(MODEL);					//pushmatrix
+	translate(MODEL, 0.0f, 1.0f, 0.0f);//apply translation to the body, in the model stack
+	rotate(MODEL, angle, 1, 0, 0);
+	sendMatrices();						//send matrices to the shader
+	drawObj(6);							//draw body
+	
+	/*	//wing1
+		sendMaterial(7);
+		pushMatrix(MODEL);			//push
+		translate(MODEL, 0.0f, 0.5f, 0);
+		sendMatrices();
+		drawObj(7);				//draw wing1
+		popMatrix(MODEL);//pop
+	*/
+	//repeat for wing2
+	popMatrix(MODEL);
+}
+
+// ------------------------------------------------------------
+//
+// Model loading and OpenGL setup
+//
+
+void init()
+{
+	// set the camera position based on its spherical coordinates
+	camX = r * sin(alpha * 3.14f / 180.0f) * cos(beta * 3.14f / 180.0f);
+	camZ = r * cos(alpha * 3.14f / 180.0f) * cos(beta * 3.14f / 180.0f);
+	camY = r *   						     sin(beta * 3.14f / 180.0f);
+
+
+	float amb[] = { 0.2f, 0.15f, 0.1f, 1.0f };
+	float diff[] = { 0.8f, 0.6f, 0.4f, 1.0f };
+	float spec[] = { 0.8f, 0.8f, 0.8f, 1.0f };
+	float emissive[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+	float shininess = 100.0f;
+	int texcount = 0;
+	
+	createCar();
+	createSnitch();
+
+	//table
+	objId=5;
+	memcpy(mesh[objId].mat.ambient, amb,4*sizeof(float));
+	memcpy(mesh[objId].mat.diffuse, diff,4*sizeof(float));
+	memcpy(mesh[objId].mat.specular, spec,4*sizeof(float));
+	memcpy(mesh[objId].mat.emissive, emissive,4*sizeof(float));
+	mesh[objId].mat.shininess = shininess;
+	mesh[objId].mat.texCount = texcount;
+	createQuad(10,10);
+
+
+	// some GL settings
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+	glEnable(GL_MULTISAMPLE);
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+}
+
+// ------------------------------------------------------------
+//
+// Main function
+//
+
+
+int main(int argc, char **argv) {
+
+	//  GLUT initialization
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA | GLUT_MULTISAMPLE);
+
+	glutInitContextVersion(3, 3);
+	glutInitContextProfile(GLUT_CORE_PROFILE);
+	glutInitContextFlags(GLUT_FORWARD_COMPATIBLE | GLUT_DEBUG);
+
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(WinX, WinY);
+	WindowHandle = glutCreateWindow(CAPTION);
+
+
+	//  Callback Registration
+	glutDisplayFunc(renderScene);
+	glutReshapeFunc(changeSize);
+	//glutIdleFunc(renderScene);
+
+//	Mouse and Keyboard Callbacks
+	glutKeyboardFunc(processKeys);
+	glutMouseFunc(processMouseButtons);
+	glutMotionFunc(processMouseMotion);
+	glutMouseWheelFunc(mouseWheel);
+	glutTimerFunc(0, timer, 0);
+	glutTimerFunc(0, refresh, 0);
+	Timer(0);
+
+
+	//	return from main loop
+	glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS);
+
+	//	Init GLEW
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	printf("Vendor: %s\n", glGetString(GL_VENDOR));
+	printf("Renderer: %s\n", glGetString(GL_RENDERER));
+	printf("Version: %s\n", glGetString(GL_VERSION));
+	printf("GLSL: %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+	if (!setupShaders())
+		return(1);
+
+	init();
+
+	//  GLUT main loop
+	glutMainLoop();
+
+	return(0);
+
+}
+
